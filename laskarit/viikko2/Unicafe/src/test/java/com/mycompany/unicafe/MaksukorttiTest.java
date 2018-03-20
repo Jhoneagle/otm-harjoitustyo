e0279d868dc671175b699be7df59b4bed208fa56
jhoneagle <eaglekjohn@gmail.com>
2018-03-20 23:00:59 +0200

    frist tests

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,45 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+
+    @Test
+    public void saldoOikein() {
+        assertTrue(this.kortti.saldo()==10);
+    }
+
+    @Test
+    public void kasvatusOikein() {
+        this.kortti.lataaRahaa(10);
+        int saldo = this.kortti.saldo();
+        assertTrue(saldo==20);
+    }
+
+    @Test
+    public void ottoOikein() {
+        //jos rahaa tarpeeksi
+        boolean onnistuiko = this.kortti.otaRahaa(10);
+        int saldo = this.kortti.saldo();
+        assertEquals(saldo, 0);
+    }
+
+    @Test
+    public void otto2Oikein() {
+        //jos rahaa tarpeeksi
+        assertTrue(this.kortti.otaRahaa(5)==true);
+    }
+
+    @Test
+    public void otto3Oikein() {
+        //jos rahaa ei tarpeeksi
+        boolean onnistuiko = this.kortti.otaRahaa(50);
+        int saldo = this.kortti.saldo();
+        assertEquals(saldo, 10);
+    }
+
+    @Test
+    public void otto4Oikein() {
+        //jos rahaa ei tarpeeksi
+        assertTrue(this.kortti.otaRahaa(50)==false);
+    }
+
 }
