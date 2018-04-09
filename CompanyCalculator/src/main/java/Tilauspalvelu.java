df299f38020c1756b560d2e3d4f210b5c7c1ac29
jhoneagle <eaglekjohn@gmail.com>
2018-04-09 13:41:23 +0300

    database created

---
+++ CompanyCalculator/src/main/java/Tilauspalvelu.java
@@ -0,0 +1,10 @@
+import database.Database;
+
+import java.sql.*;
+
+public class Tilauspalvelu {
+    public static void main(String[] args) throws Exception {
+        Database database = new Database("jdbc:sqlite:data.db");
+        
+    }
+}
